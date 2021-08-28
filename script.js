6231050b127d552cc1329bbe91b8da3106b04abe
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-08-28 11:52:00 -0700

    dynamically linked button

---
+++ script.js
@@ -0,0 +1,9 @@
+//Define variables
+
+
+//created Dom element that links to class of start button
+var buttonEl = document.querySelector("#start-quiz");
+
+buttonEl.addEventListener("click", function(){
+     alert("Welcome Earthling, the quiz will commence soon!");
+     });
