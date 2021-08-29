037a833c118245e9a45b2725c1be9869e5d2fae1
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-08-28 18:30:01 -0700

    js quiz section inserted

--- questions.js
+++ questions.js
@@ -1,94 +1,122 @@
-// defiine the variables
-var timer;
-var time = 60; //in secs//
-var questions;
+// rename to script.js
 
-//step 1: create variable to store quiz questions
-
-// creating an array and passing the number, questions, options, and answers
+//create and store questions
 var questions = [
-    {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
+  {
+     title: "What does HTML stand for?",
+     choices: [ 
+     "Hyper Text Preprocessor",
+     "Hyper Text Markup Language",
+     "Hyper Text Multiple Language",
+     "Hyper Tool Multi Language"
+],
+     answer: "Hyper Text Markup Language"
   },
-    {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
+  {
+      title: "What does CSS stand for?",
+     choices: [ 
+     "Card-Playing Style Sheet",
+     "Colorful Style Sheet",
+     "Computer-based Style Sheet",
+     "Cascading Style Sheet"
+],
+     answer: "Cascading Style Sheet"
   },
-    {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
+  {
+      title: "What is the Web API learned about in module 4?",
+     choices: ["The DOM", "jQuery", "The RON","Bootstrap"],
+     answer: "The Dom"
   },
-    {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
-    {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
-  },
+  {
+      title: "What is most common Remote repo site we have been using in class?",
+     choices: ["SlurpSpot", "GitHub", "GetHoney","SuperDooperAPPS"],
+     answer: "Github"
+  }
 ];
 
-//timer function
-
+var timer;
+var time = 60;
 function startQuiz() {
-  //Step 1: hide the start elements (Everything occurs on same page- just diff. things focused on)
+  //hide the start elements
   document.querySelector("#start-screen").classList.add("hide")
 
-  //step 2: show quiz section questions
+  //show the questions contianer
   document.querySelector("#questions").classList.remove("hide")
 
-  //start the timer  - Set interval method (refrence mdn)
+  //start the timer
   timer = setInterval(function() {
-    //countdown the time -- subtracting
-    time--;
+      //decrease time
+      time--;
 
-    //tell browser to display text of time section 
-    document.querySelector("#time").textContent = time;
+      //show the decreased time
+      document.querySelector("#time").textContent = time;
 
-    // END function - how timer will trigger end of quiz
-    if (time <= 0) { //if time is les than or equal to 0
-      endQuiz();// Endgame function 
-    }
+      //end quiz if time runs out
+      if (time <= 0) {
+          endQuiz();
+      }
   },1000);
-  
-  // Call question funtion
+
+  //show the first question
   showQuestions();
-}; 
+};
 
+document.querySelector("#start").addEventListener("click", startQuiz);
 
+var questionIndex = 0;
+function showQuestions() {
+  //create question markup
+  var questionHTML = `
+      <div class="question">
+          <h2 class="question-title">${questions[questionIndex].title}</h2>
+          <div class="choices-container">
+              <div class="question-choice">${questions[questionIndex].choices[0]}</div>
+              <div class="question-choice">${questions[questionIndex].choices[1]}</div>
+              <div class="question-choice">${questions[questionIndex].choices[2]}</div>
+              <div class="question-choice">${questions[questionIndex].choices[3]}</div>
+          </div>
+      </div>
+  `;
+
+  //convert string html into html and add it to the page
+  document.querySelector("#questions").innerHTML = questionHTML;
+
+  //assign event listener to each question choice
+  var arrayChoices = document.querySelectorAll(".question-choice");
+  for (let i = 0; i < arrayChoices.length; i++) {
+      arrayChoices[i].addEventListener("click", handleAnswerClick);
+  }
+}
+
+var score = 0;
+function handleAnswerClick(e) {
+  //check if answer is correct
+  if (e.target.textContent === questions[questionIndex].answer) {
+      score++;
+  } else {
+      time -= 10;
+  }
+
+  //show the next question
+  questionIndex++;
+
+  if (questionIndex === questions.length) {
+      endQuiz();
+  } else {
+      showQuestions();
+  }
+}
+
+function endQuiz() {
+  //stop the timer
+  clearInterval(timer);
+
+  //hide the question CredentialsContainer
+  document.querySelector("#questions").classList.add("hide");
+
+  //display the final score
+  document.querySelector("#final-score").textContent = score;
+
+  //show the end screen
+  document.querySelector("#end-screen").classList.remove("hide");
+}
